330d429b0d6cbff57b2a498592884026c778ec02
Mithun Kumar <mithunkumar123x@gmail.com>
2023-09-02 18:47:17 +0530

    adding the changes

--- dom.js
+++ dom.js
@@ -14,10 +14,75 @@ secondItem.style.color = 'green';
 
 
 //not to know how make 3rd item is invinsible ,i am doing what video taught me
-var odd= document.querySelectorAll('li:nth-child(odd)');
-var even = document.querySelectorAll('li:nth-child(even)');
 
-for(var i = 0; i < odd.length; i++){
-    odd[i].style.backgroundColor = '#f4f4f4' ;
-    even[i].style.backgroundColor = '#f4f4f4';
-}
+
+//Traversing The Dom
+var itemList = document.querySelector('#items');
+//parent Node
+//console.log(itemList.ParentNode);
+//itemList.parentNode.style.backgroundColor = '#f4f4f4';
+
+//child node
+console.log(itemList.children);
+console.log(itemList.children[1]);
+itemList.children[1].style.backgroundColor = 'fade';
+itemList.children[2].style.backgroundColor = 'blue';
+itemList.children[0].style.backgroundColor = 'red';
+itemList.children[3].style.backgroundColor = 'yellow';
+
+//FirstChild
+console.log(itemList.firstChild);
+
+// FirstElement Child
+console.log(itemList.firstElementChild);
+itemList.firstElementChild.textContent = 'HEllo word';
+
+//last child
+console.log(itemList.lastChild);
+//last Element Child
+console.log(itemList.lastElementChild);
+itemList.lastElementChild.textContent = 'Hello 4';
+
+//next sibling
+console.log(itemList.nextSibling);
+//next Elenent Sibling
+console.log(itemList.nextElementSibling);
+
+//previousSibling
+console.log(itemList.previousSibling);
+//previous Element Sibling
+console.log(itemList.previousElementSibling);
+itemList.previousElementSibling.style.color = 'green';
+
+// create element
+//create a Div
+var   newDiv = document.createElement('div');
+ // Add class
+newDiv.className = 'hello';
+
+// Add Id
+newDiv.Id = 'hello1';
+
+//Add attribute
+newDiv.setAttribute('title', 'Hello Div');
+
+//Create text Node
+var newDivText = document.createTextNode('HEllo word');
+
+// Add text to div
+newDiv.appendChild(newDivText);
+
+var container = document.querySelector('header .container');
+var h1 = document.querySelector('header h1');
+ 
+
+console.log(newDiv);
+newDiv.style.fontSize = '30px' ;
+
+container.insertBefore(newDiv , h1);
+
+
+
+
+
+
