afc0ec2cd0a449960a42a7db1eaebb853be314bc
Mithun Kumar <mithunkumar123x@gmail.com>
2023-09-09 16:39:58 +0530

    add local storage ,session ,cookie storage

--- dom.js
+++ dom.js
@@ -1,85 +1,85 @@
-//Examine the Document
-//console.dir(document);
-//console.log(document.domain);
-//console.log(document.URL);
-console.log(document.title);
-console.log(document.doctype);
-console.log(document.all[10]);
-//document.all[10].textContent='Hello';
+// //Examine the Document
+// //console.dir(document);
+// //console.log(document.domain);
+// //console.log(document.URL);
+// console.log(document.title);
+// console.log(document.doctype);
+// console.log(document.all[10]);
+// //document.all[10].textContent='Hello';
 
-// make 2nd item change color in green
-var secondItem = document.querySelector
-('.list-group-item:nth-child(2)');
-secondItem.style.color = 'green';
-
-
-//not to know how make 3rd item is invinsible ,i am doing what video taught me
+// // make 2nd item change color in green
+// var secondItem = document.querySelector
+// ('.list-group-item:nth-child(2)');
+// secondItem.style.color = 'green';
 
 
-//Traversing The Dom
-var itemList = document.querySelector('#items');
-//parent Node
-//console.log(itemList.ParentNode);
-//itemList.parentNode.style.backgroundColor = '#f4f4f4';
+// //not to know how make 3rd item is invinsible ,i am doing what video taught me
 
-//child node
-console.log(itemList.children);
-console.log(itemList.children[1]);
-itemList.children[1].style.backgroundColor = 'fade';
-itemList.children[2].style.backgroundColor = 'blue';
-itemList.children[0].style.backgroundColor = 'red';
-itemList.children[3].style.backgroundColor = 'yellow';
 
-//FirstChild
-console.log(itemList.firstChild);
+// //Traversing The Dom
+// var itemList = document.querySelector('#items');
+// //parent Node
+// //console.log(itemList.ParentNode);
+// //itemList.parentNode.style.backgroundColor = '#f4f4f4';
 
-// FirstElement Child
-console.log(itemList.firstElementChild);
-itemList.firstElementChild.textContent = 'HEllo word';
+// //child node
+// console.log(itemList.children);
+// console.log(itemList.children[1]);
+// itemList.children[1].style.backgroundColor = 'fade';
+// itemList.children[2].style.backgroundColor = 'blue';
+// itemList.children[0].style.backgroundColor = 'red';
+// itemList.children[3].style.backgroundColor = 'yellow';
 
-//last child
-console.log(itemList.lastChild);
-//last Element Child
-console.log(itemList.lastElementChild);
-itemList.lastElementChild.textContent = 'Hello 4';
+// //FirstChild
+// console.log(itemList.firstChild);
 
-//next sibling
-console.log(itemList.nextSibling);
-//next Elenent Sibling
-console.log(itemList.nextElementSibling);
+// // FirstElement Child
+// console.log(itemList.firstElementChild);
+// itemList.firstElementChild.textContent = 'HEllo word';
 
-//previousSibling
-console.log(itemList.previousSibling);
-//previous Element Sibling
-console.log(itemList.previousElementSibling);
-itemList.previousElementSibling.style.color = 'green';
+// //last child
+// console.log(itemList.lastChild);
+// //last Element Child
+// console.log(itemList.lastElementChild);
+// itemList.lastElementChild.textContent = 'Hello 4';
 
-// create element
-//create a Div
-var   newDiv = document.createElement('div');
- // Add class
-newDiv.className = 'hello';
+// //next sibling
+// console.log(itemList.nextSibling);
+// //next Elenent Sibling
+// console.log(itemList.nextElementSibling);
 
-// Add Id
-newDiv.Id = 'hello1';
+// //previousSibling
+// console.log(itemList.previousSibling);
+// //previous Element Sibling
+// console.log(itemList.previousElementSibling);
+// itemList.previousElementSibling.style.color = 'green';
 
-//Add attribute
-newDiv.setAttribute('title', 'Hello Div');
+// // create element
+// //create a Div
+// var   newDiv = document.createElement('div');
+//  // Add class
+// newDiv.className = 'hello';
 
-//Create text Node
-var newDivText = document.createTextNode('HEllo wo sfsfrd');
+// // Add Id
+// newDiv.Id = 'hello1';
 
-// Add text to div
-newDiv.appendChild(newDivText);
+// //Add attribute
+// newDiv.setAttribute('title', 'Hello Div');
 
-var container = document.querySelector('header .container');
-var h1 = document.querySelector('header h1');
+// //Create text Node
+// var newDivText = document.createTextNode('HEllo wo sfsfrd');
+
+// // Add text to div
+// newDiv.appendChild(newDivText);
+
+// var container = document.querySelector('header .container');
+// var h1 = document.querySelector('header h1');
  
 
-console.log(newDiv);
-newDiv.style.fontSize = '30px' ;
+// console.log(newDiv);
+// newDiv.style.fontSize = '30px' ;
 
-container.insertBefore(newDiv , h1);
+// container.insertBefore(newDiv , h1);
 
 
 
--- main.js
+++ main.js
@@ -1,67 +1,13 @@
-var form = document.getElementById('addForm');
-var itemList = document.getElementById('items');
-var filter = document.getElementById('filter');
+// 
+localStorage.setItem('name','Bob')
+console.log(localStorage.getItem('name'))
+localStorage.removeItem('name')
 
-// Form submit event
-form.addEventListener('submit', addItem);
-// Delete event
-itemList.addEventListener('click', removeItem);
-// Filter event
-filter.addEventListener('keyup', filterItems);
+sessionStorage.setItem('name' , 'mithun')
+console.log(sessionStorage.getItem('name'))
+sessionStorage.removeItem('name')
 
-// Add item
-function addItem(e){
-  e.preventDefault();
 
-  // Get input value
-  var newItem = document.getElementById('item').value;
+document.cookie = 'name = kayla; expire = '  + new Date(9999,0,1).toUTCString()
 
-  // Create new li element
-  var li = document.createElement('li');
-  // Add class
-  li.className = 'list-group-item';
-  // Add text node with input value
-  li.appendChild(document.createTextNode(newItem));
-
-  // Create del button element
-  var deleteBtn = document.createElement('button');
-
-  // Add classes to del button
-  deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
-
-  // Append text node
-  deleteBtn.appendChild(document.createTextNode('X'));
-
-  // Append button to li
-  li.appendChild(deleteBtn);
-
-  // Append li to list
-  itemList.appendChild(li);
-}
-
-// Remove item
-function removeItem(e){
-  if(e.target.classList.contains('delete')){
-    if(confirm('Are You Sure?')){
-      var li = e.target.parentElement;
-      itemList.removeChild(li);
-    }
-  }
-}
-
-// Filter Items
-function filterItems(e){
-  // convert text to lowercase
-  var text = e.target.value.toLowerCase();
-  // Get lis
-  var items = itemList.getElementsByTagName('li');
-  // Convert to an array
-  Array.from(items).forEach(function(item){
-    var itemName = item.firstChild.textContent;
-    if(itemName.toLowerCase().indexOf(text) != -1){
-      item.style.display = 'block';
-    } else {
-      item.style.display = 'none';
-    }
-  });
-}
+document.cookie = 'lastName = mehra; expire =' + new Date(2000,8,1).toUTCString()
